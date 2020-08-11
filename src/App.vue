75439d7bea3a838d646267de5dd57475616a34cc
Tank.tan <309313429@qq.com>
2020-08-11 21:10:00 +0800

    初始化 注册 登录 忘记密码页面

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <imooc-header />
+    <router-view />
+    <imooc-footer />
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+  name: 'App',
+  components: {
+    'imooc-header': Header,
+    'imooc-footer': Footer
   }
 }
+</script>
+
+<style lang="less">
+  @import "assets/layui/css/layui.css";
+  @import "assets/layui/css/modules/layer/default/layer.css";
+  @import "assets/css/global.css";
 </style>
