9fd08a3a464c4ff3587425362227f9e9c3f3cdf6
tank.tan <309313429@qq.com>
2020-08-25 18:44:22 +0800

    首页组件拆分

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,109 @@
 <template>
-  <div class="fly-panel fly-column">
+  <div>
+    <panel-comp />
+
     <div class="layui-container">
-      <ul class="layui-clear">
-        <li class="layui-hide-xs layui-this"><a href="/">首页</a></li>
-        <li><a href="javascript:;">提问</a></li>
-        <li><a href="javascript:;">分享<span class="layui-badge-dot"></span></a></li>
-        <li><a href="javascript:;">讨论</a></li>
-        <li><a href="javascript:;">建议</a></li>
-        <li><a href="javascript:;">公告</a></li>
-        <li><a href="javascript:;">动态</a></li>
-        <li class="layui-hide-xs layui-hide-sm layui-show-md-inline-block"><span class="fly-mid"></span></li>
+      <div class="layui-row layui-col-space15">
+        <div class="layui-col-md8">
+          <list-comp />
+        </div>
+        <div class="layui-col-md4">
 
-        <!-- 用户登入后显示 -->
-        <li class="layui-hide-xs layui-hide-sm layui-show-md-inline-block"><a href="javascript:;">我发表的贴</a></li>
-        <li class="layui-hide-xs layui-hide-sm layui-show-md-inline-block"><a href="javascript:;">我收藏的贴</a></li>
-      </ul>
+          <tips-comp />
 
-      <div class="fly-column-right layui-hide-xs">
-        <span class="fly-search"><i class="layui-icon"></i></span>
-        <a href="javascript:;" class="layui-btn">发表新帖</a>
-      </div>
-      <div class="layui-hide-sm layui-show-xs-block" style="margin-top: -10px; padding-bottom: 10px; text-align: center;">
-        <a href="javascript:;" class="layui-btn">发表新帖</a>
+          <sign-comp />
+
+          <div class="fly-panel fly-rank fly-rank-reply" id="LAY_replyRank">
+            <h3 class="fly-panel-title">回贴周榜</h3>
+            <dl>
+              <!--<i class="layui-icon fly-loading">&#xe63d;</i>-->
+              <dd>
+                <a href="user/home.html">
+                  <img src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"><cite>贤心</cite><i>106次回答</i>
+                </a>
+              </dd>
+              <dd>
+                <a href="user/home.html">
+                  <img src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"><cite>贤心</cite><i>106次回答</i>
+                </a>
+              </dd>
+              <dd>
+                <a href="user/home.html">
+                  <img src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"><cite>贤心</cite><i>106次回答</i>
+                </a>
+              </dd>
+              <dd>
+                <a href="user/home.html">
+                  <img src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"><cite>贤心</cite><i>106次回答</i>
+                </a>
+              </dd>
+              <dd>
+                <a href="user/home.html">
+                  <img src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"><cite>贤心</cite><i>106次回答</i>
+                </a>
+              </dd>
+              <dd>
+                <a href="user/home.html">
+                  <img src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"><cite>贤心</cite><i>106次回答</i>
+                </a>
+              </dd>
+              <dd>
+                <a href="user/home.html">
+                  <img src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"><cite>贤心</cite><i>106次回答</i>
+                </a>
+              </dd>
+              <dd>
+                <a href="user/home.html">
+                  <img src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"><cite>贤心</cite><i>106次回答</i>
+                </a>
+              </dd>
+              <dd>
+                <a href="user/home.html">
+                  <img src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"><cite>贤心</cite><i>106次回答</i>
+                </a>
+              </dd>
+              <dd>
+                <a href="user/home.html">
+                  <img src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"><cite>贤心</cite><i>106次回答</i>
+                </a>
+              </dd>
+              <dd>
+                <a href="user/home.html">
+                  <img src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"><cite>贤心</cite><i>106次回答</i>
+                </a>
+              </dd>
+              <dd>
+                <a href="user/home.html">
+                  <img src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"><cite>贤心</cite><i>106次回答</i>
+                </a>
+              </dd>
+            </dl>
+          </div>
+
+          <hot-list-comp />
+
+          <ads-comp />
+
+          <links-comp />
+
+        </div>
       </div>
     </div>
+
   </div>
 </template>
 
 <script>
+import PanelComp from '@/components/Panel'
+import AdsComp from '@/components/sidebar/Ads'
+import HotListComp from '@/components/sidebar/HotList'
+import LinksComp from '@/components/sidebar/Links'
+import SignComp from '@/components/sidebar/Sign'
+import TipsComp from '@/components/sidebar/Tips'
+import ListComp from '@/components/contents/List'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: { PanelComp, AdsComp, HotListComp, LinksComp, SignComp, TipsComp, ListComp }
 }
 </script>
 
