10e321bb2634393619f7c5d34e17ec257b3ebf78
tank.tan <309313429@qq.com>
2020-09-30 14:09:19 +0800

    拆分签到弹窗组件

--- src/components/sidebar/Sign.vue
+++ src/components/sidebar/Sign.vue
@@ -18,98 +18,23 @@
       <span>获得了<cite>20</cite>飞吻</span>
       -->
     </div>
-    <div class="modal" v-show="isShow">
-      <div class="mask" @click="close"></div>
-      <div class="layui-layer layui-layer-page" :class="{active: isShow}">
-        <div class="layui-layer-title">签到说明 <i @click="close" class="layui-icon layui-icon-close pull-right"></i></div>
-        <div class="layui-layer-content">
-          <div class="layui-text">
-            <blockquote class="layui-elem-quote">“签到”可获得的社区积分,规则如下</blockquote>
-            <table class="layui-table">
-              <thead>
-              <tr>
-                <td>连续签到天数</td>
-                <td>每天可获积分</td>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <td>&lt;5</td>
-                <td>5</td>
-              </tr>
-              <tr>
-                <td>&ge;5</td>
-                <td>10</td>
-              </tr>
-              <tr>
-                <td>&ge;15</td>
-                <td>15</td>
-              </tr>
-              <tr>
-                <td>&ge;30</td>
-                <td>20</td>
-              </tr>
-              <tr>
-                <td>&ge;100</td>
-                <td>30</td>
-              </tr>
-              <tr>
-                <td>&ge;365</td>
-                <td>50</td>
-              </tr>
-              </tbody>
-            </table>
-            <div>
-              <p>中间若有间隙,则连续天数重新计算</p>
-              <p class="orange">不可利用程序自动签到,否则积分清零</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
 
-    <div class="modal" v-show="showList">
-      <div class="mask" @click="close"></div>
-      <div class="layui-layer layui-layer-page" :class="{active: isShow}">
-        <div class="layui-layer-title">签到活跃榜 - TOP 20 <i @click="close" class="layui-icon layui-icon-close pull-right"></i></div>
-        <div class="layui-layer-content pd0">
-          <div class="layui-tab layui-tab-brief">
-            <ul class="layui-tab-title">
-              <li :class="{'layui-this': current === 0}" @click="choose(0)">最新签到</li>
-              <li :class="{'layui-this': current === 1}" @click="choose(1)">今日最快</li>
-              <li :class="{'layui-this': current === 2}" @click="choose(2)">总签到榜</li>
-            </ul>
-            <div class="layui-tab-content">
-              <ul class="layui-tab-item layui-show">
-                <li v-for="(item, index) in lists" :key="'sign'+index">
-                  <img src="/img/bear-200-200.jpg" alt="">
-                  <cite class="fly-link">{{item.name}}</cite>
-                  <span class="fly-grey" v-if="current!=2">签到于 {{ item.created }}</span>
-                  <span class="fly-grey" v-else>已经连续签到 <i class="orange">22</i> 天</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <sign-info :is-show="isShow" @closeModal="close"/>
+    <sign-list :is-show="showList" @closeModal="close"/>
   </div>
 </template>
 
 <script>
+import SignInfo from '@/components/sidebar/SignInfo'
+import SignList from '@/components/sidebar/SignList'
 export default {
   name: 'Sign',
+  components: { SignInfo, SignList },
   data () {
     return {
       isShow: false,
       showList: false,
-      current: 0,
-      lists: [
-        { name: 'test1', count: 1, created: '2020-10-22' },
-        { name: 'test2', count: 2, created: '2020-10-22' },
-        { name: 'test3', count: 3, created: '2020-10-22' },
-        { name: 'test4', count: 4, created: '2020-10-22' }
-      ]
+      current: 0
     }
   },
   methods: {
@@ -130,7 +55,7 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
 @keyframes bounceIn {
   0%{opacity: 0;transform: scale(0.5)}
   100%{opacity: 1;transform: scale(1)}
